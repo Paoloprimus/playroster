0b984fae8b912e61e24b2917504fd3cdfad74cc6
Paoloprimus <paolo.olivato@gmail.com>
2025-07-06 16:43:29 +0200

    Update marketStore.ts

--- stores/marketStore.ts
+++ stores/marketStore.ts
@@ -2,17 +2,24 @@
 
 import { create } from 'zustand'
 
-export const useMarketStore = create((set, get) => ({
-  budget: 500, // Budget iniziale fittizio in milioni
+type MarketStore = {
+  budget: number
+  selectedPlayer: any
+  currentBid: number | null
+  draftQueue: string[]
+  placeBid: (player: any) => void
+  joinDraft: (user: string) => void
+  confirmDraftPick: () => void
+}
 
+export const useMarketStore = create<MarketStore>((set, get) => ({
+  budget: 500,
   selectedPlayer: null,
   currentBid: null,
-
-  draftQueue: ['User A', 'User B', 'User C'], // Simulazione di utenti in coda
+  draftQueue: ['User A', 'User B', 'User C'],
 
   placeBid: (player) => {
     set({ selectedPlayer: player, currentBid: player.price })
-    // In uno step successivo: inviare offerta su Supabase
   },
 
   joinDraft: (user) => {
